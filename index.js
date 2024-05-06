60ac40bb70826c3597e3fc25fa5c73c40e7c729c
manhdeptraivcl <vanmanhn868@gmail.com>
2024-05-06 13:39:33 +0700

    90%

---
+++ index.js
@@ -0,0 +1,5 @@
+let responsive = document.querySelector('.collapse__res');
+let navbar = document.querySelector('.fa-bars');
+navbar.onclick = () =>{
+    responsive.classList.toggle('collapse__responsive')
+}
